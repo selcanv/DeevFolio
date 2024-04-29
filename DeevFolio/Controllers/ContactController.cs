87ca8bee7a7f091f115403b04396b5c097ade1fa
Selcan Vuruk <Selcanvuruk17@gmail.com>
2024-04-29 21:34:13 +0300

    ..

--- DeevFolio/Controllers/ContactController.cs
+++ DeevFolio/Controllers/ContactController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using DeevFolio.Models;
 namespace DeevFolio.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
+        DevFolioDbEntities db = new DevFolioDbEntities();
         public ActionResult Index()
         {
-            return View();
+            var values = db.TblContact.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult ReadContact(int id)
+        {
+            var value = db.TblContact.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult ReadContact()
+        {
+          
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult DeleteContact(int id)
+        {
+            var value = db.TblContact.Find(id);
+            db.TblContact.Remove(value);
+            return RedirectToAction("Index");
         }
         public ActionResult SendMessage()
         {
